8a0d30085c1a7ba394959c2f79ae07ec144eb73d
Michal Bartosik <msbartosik@gmail.com>
2021-04-09 11:59:13 +0200

    fix url

--- src/helpers/api.js
+++ src/helpers/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const baseUrl = 'http://static-job-listings-backend.vercel.app/api'
+const baseUrl = 'https://static-job-listings-backend.vercel.app/api'
 
 const fetchJobs = async () => {
   const request = await axios.get(`${baseUrl}`)
